cb974a2c4025413ad5686e3dd0f0be90e96ac144
Skial Bainn <skial.bainn@gmail.com>
2018-11-17 17:05:05 +0000

    Disable HashLink for now. No good random byte generator as far as I can find. Update SecureRandom.hx a little more.

--- src/uhx/uid/util/SecureRandom.hx
+++ src/uhx/uid/util/SecureRandom.hx
@@ -1,70 +1,58 @@
 package uhx.uid.util;
 
-import haxe.io.BytesData;
 import haxe.io.Bytes;
 
 // Taken from haxe-crypto
 // @see https://github.com/soywiz/haxe-crypto/blob/master/src/com/hurlant/crypto/prng/SecureRandom.hx
 class SecureRandom {
-    // @TODO: IMPORTANT!!
-    /**
-     * Gather anything we have that isn't entirely predictable:
-     *  - memory used
-     *  - system capabilities
-     *  - timing stuff
-     *  - installed fonts
-     */
-    static public function random(length:Int):Bytes {
-        var reason = '';
-        try {
-            #if flash
-                return Bytes.ofData(untyped __global__["flash.crypto.generateRandomBytes"](length));
-            #elseif js
-                #if hxnodejs
-                return js.node.Crypto.randomBytes(length).hxToBytes();
-                #else
-                return Bytes.ofData( js.Browser.window.crypto.getRandomValues( new js.html.Uint8Array(Bytes.alloc(length).getData()) ).buffer );
-                /*untyped __js__('var Crypto = typeof crypto === "undefined" ? require("crypto") : crypto');
-                var bytes:Dynamic = untyped __js__("(Crypto.randomBytes) ? Crypto.randomBytes({0}) : Crypto.getRandomValues(new Uint8Array({0}))", length);
-                var out = Bytes.alloc(length);
-                for (n in 0 ... length) out.set(n, bytes[n]);
-                return out;*/
-                #end
-            #elseif python
-                var out = Bytes.alloc(length);
-                var bytes = RandomOs.urandom(length);
-                for (n in 0 ... length) out.set(n, bytes[n]);
-                return out;
-            #elseif java
-                return Bytes.ofData(java.security.SecureRandom.getSeed(length));
-            #elseif cs
-                var out = Bytes.alloc(length);
-                var rng = new cs.system.security.cryptography.RNGCryptoServiceProvider();
-                rng.GetBytes(out.getData());
-                return out;
-            #elseif sys
-                // https://en.wikipedia.org/wiki//dev/random
-                var out = Bytes.alloc(length);
-                #if windows
-                    var input = sys.io.File.read("\\Device\\KsecDD");
-                #else
-                    var input = sys.io.File.read("/dev/urandom");
-                #end
-                input.readBytes(out, 0, length);
-                input.close();
-                return out;
+    public static inline function random(length:Int):Bytes {
+        #if flash
+            return Bytes.ofData(untyped __global__["flash.crypto.generateRandomBytes"](length));
+        #elseif js
+            #if hxnodejs
+            return js.node.Crypto.randomBytes(length).hxToBytes();
+            #else
+            return Bytes.ofData( js.Browser.window.crypto.getRandomValues( new js.html.Uint8Array(Bytes.alloc(length).getData()) ).buffer );
             #end
-        } catch (e:Any) {
-            reason = '$e';
-        }
-        throw "Can't find a secure source of random bytes. Reason: " + reason;
+        #elseif php
+            // http://php.net/manual/en/function.random-bytes.php
+            return Bytes.ofData( (php.Syntax.code('random_bytes({0})', length):String) );
+        #elseif python
+            return Bytes.ofData( OS.urandom(length) );
+        #elseif java
+            return Bytes.ofData(java.security.SecureRandom.getSeed(length));
+        #elseif cs
+            var out = Bytes.alloc(length);
+            var rng = new cs.system.security.cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(out.getData());
+            return out;
+        #elseif hl
+            #error "HashLink is not supported";
+        #elseif sys
+            // https://en.wikipedia.org/wiki//dev/random
+            var out = Bytes.alloc(length);
+            var input = sys.io.File.read( (Sys.systemName() == 'Windows') ? "\\Device\\KsecDD" : "/dev/urandom");
+            input.readBytes(out, 0, length);
+            input.close();
+            return out;
+        #end
     }
+}
+
+#if hl
+@:hlNative("ssl")
+class SSL {
 
 }
+/*
+mbedtls_ctr_drbg_random
+mbedtls_hmac_drbg_random
+*/
+#end
 
 #if python
 @:pythonImport("os")
-extern class RandomOs {
-    static public function urandom(count:Int):Array<Int>;
+extern class OS {
+    static public function urandom(count:Int):python.Bytes;
 }
 #end
